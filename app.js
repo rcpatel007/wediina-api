ce2b05dcd10556f4cdf0e460ca6f299ddbc9e9cc
Trinomads <trinomadsglobal@gmail.com>
2019-09-14 08:37:21 +0530

    update

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false, limit: '100000mb' }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-// res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'DELETE, HEAD, GET, OPTIONS, POST, PUT, delete, put, post, post, options, get');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token");
   next();
 });
 
